a9d366e40884840cc03bbdafe5d0e42560dda821
Javier <jmartin@grupoloyal.com>
2018-12-12 09:42:32 +0100

    estilo

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,15 +11,15 @@ export class HomeComponent implements OnInit {
   title = 'Hola Mundo';
 
   constructor(notify: NotificationService, out: LoggerService) {
-    notify.add('Esto es una notificacion de error');
-    notify.add('Esto es una notificacion de aviso', NotificationType.warn);
-    notify.remove(0);
-    notify.add(null);
-    notify.remove(0);
-    out.error('Esto es un error');
-    out.warn('Esto es un warn');
-    out.info('Esto es un info');
-    out.log('Esto es un log');
+    // notify.add('Esto es una notificacion de error');
+    // notify.add('Esto es una notificacion de aviso', NotificationType.warn);
+    // notify.remove(0);
+    // notify.add(null);
+    // notify.remove(0);
+    // out.error('Esto es un error');
+    // out.warn('Esto es un warn');
+    // out.info('Esto es un info');
+    // out.log('Esto es un log');
   }
   ngOnInit() {
   }
