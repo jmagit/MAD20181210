05e3204367023cc585049df09b37932267f373ef
Javier <jmartin@grupoloyal.com>
2018-12-12 17:23:50 +0100

    validadores

--- src/indra-core/indra-core.module.ts
+++ src/indra-core/indra-core.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { PIPES_CADENAS } from './pipes/cadenas.pipe';
 import { IndraSizerComponent } from './components/indra-sizer.component';
 import { PIPES_NUMERICOS } from './pipes/numericos.pipe';
+import { MIS_VALIDADORES } from './directives/mis-validadores.directive';
 
 @NgModule({
-  declarations: [ PIPES_CADENAS, PIPES_NUMERICOS, IndraSizerComponent ],
-  exports: [ PIPES_CADENAS, PIPES_NUMERICOS, IndraSizerComponent ],
+  declarations: [ PIPES_CADENAS, PIPES_NUMERICOS, MIS_VALIDADORES, IndraSizerComponent ],
+  exports: [ PIPES_CADENAS, PIPES_NUMERICOS, MIS_VALIDADORES, IndraSizerComponent ],
   imports: [
     CommonModule
   ],
